6882fb7aa289c9fbdd6bcb474dc05f9e81385991
mzp <mzpppp@gmail.com>
2016-06-29 12:50:43 +0900

    bundle gem prpr-trello

---
+++ lib/prpr/trello.rb
@@ -0,0 +1,7 @@
+require "prpr/trello/version"
+
+module Prpr
+  module Trello
+    # Your code goes here...
+  end
+end
---
+++ lib/prpr/trello/version.rb
@@ -0,0 +1,5 @@
+module Prpr
+  module Trello
+    VERSION = "0.1.0"
+  end
+end
